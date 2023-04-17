b8379361234ae01e7cc02446ff07e7c77a7b2d15
Stephan Bösebeck <sb@caluga.de>
2023-04-17 21:05:22 +0200

    moving to cli-tool

---
+++ src/main/java/de/caluga/morpheus/Morpheus.java
@@ -0,0 +1,14 @@
+package de.caluga.morpheus;
+
+
+public class Morpheus {
+
+    public static void main(String args[]) throws Exception {
+        // ASCIIArt arg=new ASCIIArt();
+        // arg.printTextArt("Morpheus",15 );
+        System.out.println("Started...");
+        Thread.sleep(1000);
+        System.out.println("IT works...");
+    }
+
+}
