7ab43bb8e6592dadd93c9f8ba3f726a804e3a600
Stephan Bösebeck <sb@caluga.de>
2026-05-09 22:42:00 +0200

    adding some commands

---
+++ src/main/java/de/caluga/morpheus/commands/ListCommands.java
@@ -0,0 +1,21 @@
+package de.caluga.morpheus.commands;
+
+import java.util.List;
+import java.util.Map;
+import de.caluga.morpheus.ICommand;
+import de.caluga.morpheus.Morpheus;
+
+public class ListCommands implements ICommand {
+    public final static String NAME = "list";
+    public final static String DESCRIPTION = "This little command takes no arguments, just lists all existing commands";
+
+    @Override
+    public void execute(Morpheus morpheus, Map<String, String> args) throws Exception {
+        for (var cls : morpheus.getCommandClasses()) {
+            String name = morpheus.getNameFromCommandClass(cls);
+            String desc = morpheus.getDocumentationFromCommandClass(cls);
+        }
+
+    }
+
+}
