22ffc84609628ec12260cf1f97d0f6fe3e4bf2b3
Stephan Bösebeck <sb@caluga.de>
2026-05-10 23:18:00 +0200

    statut output

--- src/main/java/de/caluga/morpheus/commands/GetStatus.java
+++ src/main/java/de/caluga/morpheus/commands/GetStatus.java
@@ -1,7 +1,9 @@
 package de.caluga.morpheus.commands;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import org.json.simple.parser.JSONParser;
 import de.caluga.morpheus.ICommand;
 import de.caluga.morpheus.Morpheus;
 import de.caluga.morphium.driver.MorphiumId;
@@ -13,11 +15,26 @@ public class GetStatus implements ICommand {
 
     @Override
     public void execute(Morpheus morpheus, Map<String, String> args) throws Exception {
-        morpheus.pr("[c1]sending status ping[r]....(waiting 30s for answers)");
+        int sl=30;
+        if (args.containsKey("wait")){
+            sl=Integer.parseInt(args.get("wait"));
+        }
+        morpheus.pr("[c1]sending status ping[r]....(waiting "+sl+"s for answers)");
         long sendTS = System.currentTimeMillis();
-        var msg = new Msg(morpheus.getMessaging().getStatusInfoListenerName(), "ALL", "ALL", 30000);
+        final int timeout=sl;
+        new Thread(()->{
+            for (int i=0;i<timeout;i++){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+                System.out.print("\u001B[1A\r");
+                morpheus.pr(""+i);
+            }
+        }).start();
+        var msg = new Msg(morpheus.getMessaging().getStatusInfoListenerName(), "ALL", "ALL", sl*1000);
         msg.setMsgId(new MorphiumId());
-        var results = morpheus.getMessaging().sendAndAwaitAnswers(msg, 40, 15000);
+        var results = morpheus.getMessaging().sendAndAwaitAnswers(msg, 40, sl*1000);
         morpheus.pr("[header1]got messages:[r]" + results.size());
         boolean verbose = false;
 
@@ -42,14 +59,29 @@ public class GetStatus implements ICommand {
                 if (r.getMapValue() != null) {
                     for (Map.Entry<String, Object> k : r.getMapValue().entrySet()) {
                         if (k.getKey().equals("message_listeners_by_name")) {
-                            morpheus.pr("         [c2]registered Listeners:[r]");
+                            morpheus.pr("      "+morpheus.getColumn("[c3]registered Listeners:[r]",25));
                             String[] lst = k.getValue().toString().replaceAll("[{}]+", "").split(",");
 
                             for (String l : lst) {
-                                morpheus.pr("            [good]" + l.substring(0, l.indexOf("=")) + "[r]");
+                                morpheus.pr(morpheus.getColumn("", 34)+"[good]" + l.substring(0, l.indexOf("=")) + "[r]");
                             }
+                        } else if (k.getValue() instanceof List){
+                            if (((List)k.getValue()).isEmpty()) continue;
+                            morpheus.pr(morpheus.getColumn("[good]"+k.getKey()+"[r]", 25));
+
+                            for (Object l : (List)k.getValue()) {
+                                morpheus.pr(morpheus.getColumn("", 34)+"[good]" + l.toString() + "[r]");
+                            }
+                            // morpheus.pr("      [c3]" + morpheus.getColumn(k.getKey(), 25) + "[r] List!");
+                        } else if (k.getValue() instanceof Map){
+                            morpheus.pr("      [c3]" + morpheus.getColumn(k.getKey(), 35) + "[r]");
+                                Map<String,Object> m=(Map<String,Object>)k.getValue();
+                                for (var e:m.entrySet()){
+                                    morpheus.pr("      "+morpheus.getColumn("", 35)+" [good]"+morpheus.getColumn(e.getKey(),35)+":[r]  "+e.getValue());
+                                }
+
                         } else {
-                            morpheus.pr("      [c3]" + morpheus.getColumn(k.getKey(), 25) + "[r] | " + k.getValue());
+                            morpheus.pr("      [c3]" + morpheus.getColumn(k.getKey(), 35) + "[r] | " + k.getValue());
                         }
                     }
                 }
